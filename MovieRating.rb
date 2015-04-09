5de585c2c7adbfc50956dceb1dbc83e82432efb3
Juan <juan.antonadelval@gmail.com>
2015-04-10 00:59:38 +0200

    graphic_pseudocode

--- MovieRating.rb
+++ MovieRating.rb
@@ -1,11 +1,14 @@
 require "imdb"
 require "Pry"
 
+##########################
+#CLASS DECLARATION
+##########################
+
 class IMDBRating
 
   def initialize
    @movies = []
-   @search = []
    @rating = []
   end	
   
@@ -15,12 +18,24 @@ class IMDBRating
 
   def rating
     process_file.each do |movie|
-       @rating << Imdb::Movie.new("#{Imdb::Search.new(movie).movies[0].id}").rating
+       @rating << Imdb::Movie.new("#{Imdb::Search.new(movie).movies[0].id}").rating.to_i
     end
+    puts @rating 
   end
-  
+
+  def format
+  	for i = @rating.maxvalue to 1 by -1
+  	  @movies.each do |movie|
+  	  	puts "|" + [if movie.rating > i "#"," "] + "|"
+  	  end	  
+    end	
+  end
 
 end
+
+##########################
+#MAIN
+##########################
 
 IMDBRating.new.rating	
 
